905f6eac910e7cb7aa1ca9943bc0eff221934fd8
piomin <piotr.minkowski@gmail.com>
2020-12-06 00:16:05 +0100

    Adding new version of Camel and Spring Boot

--- camel-common/src/main/java/pl/piomin/services/camel/common/model/Account.java
+++ camel-common/src/main/java/pl/piomin/services/camel/common/model/Account.java
@@ -1,53 +1,19 @@
 package pl.piomin.services.camel.common.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
 
 	private Integer id;
 	private String number;
 	private int amount;
 	private Integer customerId;
-
-	public Account() {
-
-	}
-
-	public Account(Integer id, String number, int amount, Integer customerId) {
-		this.id = id;
-		this.number = number;
-		this.amount = amount;
-		this.customerId = customerId;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	public void setNumber(String number) {
-		this.number = number;
-	}
-
-	public int getAmount() {
-		return amount;
-	}
-
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-
-	public Integer getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(Integer customerId) {
-		this.customerId = customerId;
-	}
 
 }
--- camel-common/src/main/java/pl/piomin/services/camel/common/model/Customer.java
+++ camel-common/src/main/java/pl/piomin/services/camel/common/model/Customer.java
@@ -2,59 +2,20 @@ package pl.piomin.services.camel.common.model;
 
 import java.util.List;
 
-import pl.piomin.services.camel.common.model.Account;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 
 	private Integer id;
 	private String name;
 	private String pesel;
 	private List<Account> accounts;
-
-	public Customer() {
-
-	}
-
-	public Customer(Integer id, String name, String pesel) {
-		this.id = id;
-		this.name = name;
-		this.pesel = pesel;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPesel() {
-		return pesel;
-	}
-
-	public void setPesel(String pesel) {
-		this.pesel = pesel;
-	}
-
-	public List<Account> getAccounts() {
-		return accounts;
-	}
-
-	public void setAccounts(List<Account> accounts) {
-		this.accounts = accounts;
-	}
-
-	public void addAccount(Account account) {
-		this.accounts.add(account);
-	}
 	
 }
