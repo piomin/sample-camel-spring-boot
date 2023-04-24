7542dbb1f905469ef4887b7a1ee8a9ad39922d56
piomin <piotr.minkowski@gmail.com>
2023-04-24 12:08:23 +0200

    adding hoverfly tests

--- customer/src/test/java/pl/piomin/services/camel/customer/CustomerRouteTests.java
+++ customer/src/test/java/pl/piomin/services/camel/customer/CustomerRouteTests.java
@@ -1,14 +1,21 @@
 package pl.piomin.services.camel.customer;
 
+import io.specto.hoverfly.junit.core.Hoverfly;
+import io.specto.hoverfly.junit5.HoverflyExtension;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import pl.piomin.services.camel.common.model.Customer;
 
+import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
+import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
+
 @SpringBootTest
-//@ExtendWith(HoverflyExtension.class)
+@ExtendWith(HoverflyExtension.class)
 public class CustomerRouteTests {
 
     TestRestTemplate restTemplate = new TestRestTemplate();
@@ -27,16 +34,16 @@ public class CustomerRouteTests {
         Assertions.assertTrue(customers.length > 0);
     }
 
-//	@Test
-//	public void findById(Hoverfly hoverfly) {
-//        hoverfly.simulate(
-//                dsl(service("http://account:8080")
-//                        .get("/")
-//                        .willReturn(success("[{\"id\":\"1\",\"number\":\"1234567890\"}]", "application/json"))
-//        ));
-//		Customer a = restTemplate.getForObject("http://localhost:8080/customer/1", Customer.class);
-//		Assertions.assertNotNull(a);
-//		Assertions.assertEquals(1, a.getId());
-//	}
+	@Test
+	public void findById(Hoverfly hoverfly) {
+        hoverfly.simulate(
+                dsl(service("http://account:8080")
+                        .get("/customer/1")
+                        .willReturn(success("[{\"id\":\"1\",\"number\":\"1234567890\"}]", "application/json"))
+        ));
+		Customer a = restTemplate.getForObject("http://localhost:8080/customer/1", Customer.class);
+		Assertions.assertNotNull(a);
+		Assertions.assertEquals(1, a.getId());
+	}
 
 }
