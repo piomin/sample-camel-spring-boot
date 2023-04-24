291a9bb380d2f3e668eafe36e65db00cc4731e53
piomin <piotr.minkowski@gmail.com>
2023-04-24 12:02:45 +0200

    adding tests

--- customer/src/main/java/pl/piomin/services/camel/customer/CustomerRoute.java
+++ customer/src/main/java/pl/piomin/services/camel/customer/CustomerRoute.java
@@ -14,44 +14,44 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerRoute extends RouteBuilder {
-		
-	@Autowired
-	CamelContext context;
-			
-	@Override
-	public void configure() throws Exception { 
-		
-		JacksonDataFormat format = new JacksonDataFormat();
-		format.useList();
-		format.setUnmarshalType(Account.class);
-		
-		restConfiguration()
-			.component("netty-http")
-			.bindingMode(RestBindingMode.json)
-			.port(8080);
-		
+
+    @Autowired
+    CamelContext context;
+
+    @Override
+    public void configure() throws Exception {
+
+        JacksonDataFormat format = new JacksonDataFormat();
+        format.useList();
+        format.setUnmarshalType(Account.class);
+
+        restConfiguration()
+                .component("netty-http")
+                .bindingMode(RestBindingMode.json)
+                .port(8080);
+
 //		from("direct:start").routeId("account-consul").marshal().json(JsonLibrary.Jackson)
 //			.setHeader(Exchange.HTTP_METHOD, constant("PUT"))
 //			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
 //			.to("http://192.168.99.100:8500/v1/agent/service/register");
 //		from("direct:stop").shutdownRunningTask(ShutdownRunningTask.CompleteAllTasks)
 //			.toD("http://192.168.99.100:8500/v1/agent/service/deregister/${header.id}");
-		
-		rest("/customer")
-			.get("/")
-				.to("bean:customerService?method=findAll")
-			.post("/").consumes("application/json").type(Customer.class)
-				.to("bean:customerService?method=add(${body})")
-			.get("/{id}").to("direct:account").produces("application/json");
-		
-		
-		from("direct:account")
-			.to("bean:customerService?method=findById(${header.id})")
-			.log("Msg: ${body}").enrich("direct:acc", new AggregationStrategyImpl());
-		
-		
-		from("direct:acc").setBody().constant(null).to("http://account:8080").unmarshal(format);
-			
-	}
-		
+
+        rest("/customer")
+            .get("/")
+                .to("bean:customerService?method=findAll")
+            .post("/").consumes("application/json").type(Customer.class)
+                .to("bean:customerService?method=add(${body})")
+            .get("/{id}").to("direct:account").produces("application/json");
+
+
+        from("direct:account")
+                .to("bean:customerService?method=findById(${header.id})")
+                .log("Msg: ${body}").enrich("direct:acc", new AggregationStrategyImpl());
+
+
+        from("direct:acc").setBody().constant(null).to("http://account:8080").unmarshal(format);
+
+    }
+
 }
--- customer/src/main/java/pl/piomin/services/camel/customer/service/CustomerService.java
+++ customer/src/main/java/pl/piomin/services/camel/customer/service/CustomerService.java
@@ -10,23 +10,23 @@ import pl.piomin.services.camel.common.model.Customer;
 @Service
 public class CustomerService {
 
-	private List<Customer> customers = new ArrayList<>();
-	
-	public Customer findById(Integer id) {
-		return new Customer(id, "XXX", "1234567890", null);
-	}
-	
-	public List<Customer> findAll() {
-		List<Customer> l = new ArrayList<>();
-		l.add(new Customer(1, "XXX", "1234567890", null));
-		l.add(new Customer(2, "YYY", "1234567891", null));
-		return l;
-	}
-	
-	public Customer add(Customer customer) {
-		customer.setId(customers.size()+1);
-		customers.add(customer);
-		return customer;
-	}
-	
+    private List<Customer> customers = new ArrayList<>();
+
+    public Customer findById(Integer id) {
+        return new Customer(id, "XXX", "1234567890", null);
+    }
+
+    public List<Customer> findAll() {
+        List<Customer> l = new ArrayList<>();
+        l.add(new Customer(1, "XXX", "1234567890", null));
+        l.add(new Customer(2, "YYY", "1234567891", null));
+        return l;
+    }
+
+    public Customer add(Customer customer) {
+        customer.setId(customers.size() + 1);
+        customers.add(customer);
+        return customer;
+    }
+
 }
